1977cd0dd93519827065453a52be0d756c3dd711
ankit.jan9661@gmail.com <ankit.jan9661@gmail.com>
2025-10-27 17:51:12 +0530

    initial changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,7 +9,6 @@ export const authenticate = async (req, res, next) => {
     if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ message: "No token provided or invalid format" });
     }
-
     const token = authHeader.split(" ")[1];
     const decoded = verifyToken(token);
 
